d7781cfa6ae078c42359516c559590f5afa62b6d
Jose M Viloria <josevill.linuxusr@gmail.com>
2015-09-10 16:30:24 -0430

    gh-pages - Working version v1

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-	
+// In case of refreshing the site, we're going to clean up the input
+$('#add-val').val("");
+  $("#send").click(function() {
+    var userInput = $("#add-val").val();
+    if(userInput !== ""){
+      $('ul').append("<li id='item' class='list-group-item'>" + userInput + "</li>");
+      $('#add-val').val("");
+    } else {
+      alert("Empty input!");
+    };
+  });
+
+  // Event handling through .on() using doubleclick selecting li elements then using some fancy animations to make elements dissapear.
+  $('ul').on('dblclick', 'li', function() {
+    $(this).slideUp();
+  });
 });
